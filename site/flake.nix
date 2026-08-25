e3ada589f24438201e5a08297894c099de22e8fc
Can H. Tartanoglu <gpg@rotas.mozmail.com>
2026-08-25 02:12:50 +0200

    build: use GitHub-clean Harbor pin

--- site/flake.nix
+++ site/flake.nix
@@ -11,7 +11,7 @@
     };
 
     harbor-rs = {
-      url = "git+https://github.com/caniko/harbor-rs.git?ref=trunk&rev=f209ddbca3fdbb0dc31fa3886ccc2ff7369c18ac";
+      url = "git+https://github.com/caniko/harbor-rs.git?ref=trunk&rev=05cc4f162b55fa904b687db1821e2463fa813e50";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     rs-harbor.follows = "harbor-rs";
