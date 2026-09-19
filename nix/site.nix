7c9ad1d1a71e425e316a32542842aa06b8f22246
Can H. Tartanoglu <gpg@rotas.mozmail.com>
2026-09-19 16:30:25 +0200

    refactor(nix): build docs and site through harbor-docs
    Adopt harbor-docs.mkDocs and harbor-docs.mkSite in place of the
    inline mdBook derivation and direct mkProjectSite call. The docs
    derivation hash is unchanged (verified identical); site wiring
    passes the same domain, config, static paths, and docs package.
    
    Also renames website/static/rs-harbor-mark.svg to harbor-rs-mark.svg,
    which every reference already used since the project rename.

--- nix/site.nix
+++ nix/site.nix
@@ -2,29 +2,19 @@
   pkgs,
   lib,
   projectSiteLib,
+  harborDocs,
 }: let
-  docs = pkgs.stdenv.mkDerivation {
-    pname = "harbor-rs-docs";
-    version = "0.1.0";
+  docs = harborDocs.mkDocs {
+    inherit pkgs;
     src = ../docs;
-    nativeBuildInputs = [pkgs.mdbook];
-    phases = ["buildPhase" "installPhase"];
-    buildPhase = ''
-      cp -r --no-preserve=mode $src docs
-      chmod -R u+w docs
-      mdbook build docs
-    '';
-    installPhase = ''
-      mkdir -p $out
-      cp -r docs/book/. $out/
-    '';
+    pname = "harbor-rs-docs";
   };
 
-  website = projectSiteLib.mkProjectSite {
+  website = harborDocs.mkSite {
+    inherit projectSiteLib docs;
     pname = "harbor-rs-website";
     domain = "harbor-rs.tartanoglu.com";
     configPath = ../website/plinth-project.toml;
-    docsPackage = docs;
     staticPaths = [
       {
         source = ../website/static/harbor-rs-mark.svg;
